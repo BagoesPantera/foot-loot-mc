f1622be3bfe58354cacced885b8d4bb9bce444f1
pantera <bagoespantera01@gmail.com>
2026-02-14 18:51:27 +0800

    fix: replace block drops with block items for player trail

--- src/main/java/com/sakunoki/mixin/ServerTickMixin.java
+++ src/main/java/com/sakunoki/mixin/ServerTickMixin.java
@@ -1,10 +1,10 @@
 package com.sakunoki.mixin;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
@@ -22,21 +22,25 @@ public class ServerTickMixin {
 	@Inject(at = @At("RETURN"), method = "tick")
 	private void onTick(BooleanSupplier shouldKeepTicking, CallbackInfo info) {
 		MinecraftServer server = (MinecraftServer) (Object) this;
-		if (server.getTicks() % 5 != 0) return;
+		if (server.getTicks() % 5 != 0)
+			return;
 
 		for (ServerPlayerEntity player : server.getPlayerManager().getPlayerList()) {
-			if (player.isSpectator()) continue;
+			if (player.isSpectator())
+				continue;
 
 			BlockPos pos = player.getBlockPos().down();
 			ServerWorld world = player.getServerWorld();
 			BlockState state = world.getBlockState(pos);
 
 			if (!state.isAir() && !state.isOf(Blocks.BEDROCK)) {
-				List<ItemStack> drops = Block.getDroppedStacks(state, world, pos, null);
-				for (ItemStack stack : drops) {
-					ItemEntity itemEntity = new ItemEntity(world, pos.getX() + 0.5, pos.getY() + 1.0, pos.getZ() + 0.5, stack.copy());
-					itemEntity.setToDefaultPickupDelay();
-					world.spawnEntity(itemEntity);
+				var item = state.getBlock().asItem();
+				if (item != Items.AIR) {
+					ItemStack stack = new ItemStack(item);
+					ItemEntity entity = new ItemEntity(world, pos.getX() + 0.5, pos.getY() + 1.0, pos.getZ() + 0.5,
+							stack);
+					entity.setToDefaultPickupDelay();
+					world.spawnEntity(entity);
 				}
 			}
 		}
